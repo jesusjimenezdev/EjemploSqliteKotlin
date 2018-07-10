c9a7cb55b2f853e58ce6880efc4c6daf2550085e
jesus <info@jesusjimenez.net>
2018-07-10 19:01:02 +0200

    commit final

--- app/src/main/java/jesus/net/ejemplosqlitekotlin/AddActivity.kt
+++ app/src/main/java/jesus/net/ejemplosqlitekotlin/AddActivity.kt
@@ -1,17 +1,52 @@
 package jesus.net.ejemplosqlitekotlin
 
+import android.content.ContentValues
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_add.*
 
 class AddActivity : AppCompatActivity() {
+
+    var id = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add)
 
-        btnAgregar.setOnClickListener {
+        try {
+            val bundle: Bundle = intent.extras
+            id = bundle.getInt("ID", 0)
+            val control = bundle.getInt("control")
+            if(id != 0 && control == 0) {
+                editTextTitulo.setText(bundle.getString("titulo"))
+                editTextContenido.setText(bundle.getString("descripcion"))
+                btnAgregar.setText("ACTUALIZAR")
+            }
+        }catch (e: Exception){}
 
+
+        btnAgregar.setOnClickListener {
+            val baseDatos = DBManager(this)
+            val values = ContentValues()
+            values.put("Titulo", editTextTitulo.text.toString())
+            values.put("Descripcion", editTextContenido.text.toString())
+            if(id == 0) {
+                val ID = baseDatos.insert(values)
+                if (ID > 0) {
+                    Toast.makeText(this, "Nota agregada correctamente", Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(this, "Nota no agregada correctamente", Toast.LENGTH_LONG).show()
+                }
+            } else {
+                val selectionArgs = arrayOf(id.toString())
+                val ID = baseDatos.actualizar(values, "ID=?", selectionArgs)
+                if (ID > 0) {
+                    Toast.makeText(this, "Nota agregada correctamente", Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(this, "Nota no agregada correctamente", Toast.LENGTH_LONG).show()
+                }
+            }
         }
     }
 }
--- app/src/main/java/jesus/net/ejemplosqlitekotlin/MainActivity.kt
+++ app/src/main/java/jesus/net/ejemplosqlitekotlin/MainActivity.kt
@@ -20,12 +20,26 @@ class MainActivity(var adapter: NotasAdapter? = null) : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        listaDeNotas.add(Nota(1, "Nota 1", "Descripcion de la nota"))
-        listaDeNotas.add(Nota(2, "Nota 2", "Descripcion de la nota"))
-        listaDeNotas.add(Nota(3, "Nota 3", "Descripcion de la nota"))
+        // Ponemos el % para que cargue todas las notas
+        cargarQuery("%")
 
-        adapter = NotasAdapter(this, listaDeNotas)
-        lista.adapter = adapter
+        /*listaDeNotas.add(Nota(1, "Nota 1", "Descripcion de la nota"))
+        listaDeNotas.add(Nota(2, "Nota 2", "Descripcion de la nota"))
+        listaDeNotas.add(Nota(3, "Nota 3", "Descripcion de la nota"))*/
+
+        /*adapter = NotasAdapter(this, listaDeNotas)
+        lista.adapter = adapter*/
+
+        fab.setOnClickListener {
+            var intent = Intent(this, AddActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    // onResume() se ejecuta cuando pasamos de una actividad a otra
+    override fun onResume() {
+        super.onResume()
+        cargarQuery("%")
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -33,19 +47,42 @@ class MainActivity(var adapter: NotasAdapter? = null) : AppCompatActivity() {
         val buscar = menu!!.findItem(R.id.app_bar_search).actionView as SearchView
         val manejador = getSystemService(Context.SEARCH_SERVICE) as SearchManager
         buscar.setSearchableInfo(manejador.getSearchableInfo(componentName))
+
         buscar.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
 
             override fun onQueryTextSubmit(texto: String?): Boolean {
                 Toast.makeText(applicationContext, texto, Toast.LENGTH_SHORT).show()
+                cargarQuery("%" + texto + "%")
                 return false
             }
 
             override fun onQueryTextChange(texto: String?): Boolean {
+                cargarQuery("%" + texto + "%")
                 return false
             }
 
         })
         return super.onCreateOptionsMenu(menu)
+    }
+
+    // SELECT ID, TITULO, DESCRIPCION FROM NOTAS WHERE (TITULO LIKE ?) ORDER BY TITULO
+    fun cargarQuery(titulo: String) {
+        var baseDatos = DBManager(this)
+        val columnas = arrayOf("ID", "Titulo", "Descripcion")
+        val selectionArgs = arrayOf(titulo)
+        val cursor = baseDatos.query(columnas, "Titulo LIKE ?", selectionArgs, "Titulo")
+        listaDeNotas.clear()
+        if(cursor.moveToFirst()) {
+            do {
+                val ID = cursor.getInt(cursor.getColumnIndex("ID"))
+                val titulo = cursor.getString(cursor.getColumnIndex("Titulo"))
+                val descripcion = cursor.getString(cursor.getColumnIndex("Descripcion"))
+                listaDeNotas.add(Nota(ID, titulo, descripcion))
+            }while (cursor.moveToNext())
+        }
+
+        adapter = NotasAdapter(this, listaDeNotas)
+        lista.adapter = adapter
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
@@ -58,7 +95,7 @@ class MainActivity(var adapter: NotasAdapter? = null) : AppCompatActivity() {
         return super.onOptionsItemSelected(item)
     }
 
-    class NotasAdapter(contexto: Context, var listaDeNotas: ArrayList<Nota>): BaseAdapter() {
+    inner class NotasAdapter(contexto: Context, var listaDeNotas: ArrayList<Nota>): BaseAdapter() {
 
         var contexto: Context = contexto
 
@@ -75,11 +112,33 @@ class MainActivity(var adapter: NotasAdapter? = null) : AppCompatActivity() {
         }
 
         override fun getView(index: Int, view: View?, viewGroup: ViewGroup?): View {
+            var convertView: View? = view
+            if(convertView == null) {
+                convertView = View.inflate(contexto, R.layout.molde_notas, null)
+            }
+
             val nota = listaDeNotas[index]
-            val inflater = contexto.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-            val miVista = inflater.inflate(R.layout.molde_notas, null)
+            //val inflater = contexto.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            val miVista = convertView!! //inflater.inflate(R.layout.molde_notas, null)
             miVista.textViewTitulo.text = nota.titulo
             miVista.textViewContenido.text = nota.descripcion
+
+            miVista.imageViewBorrar.setOnClickListener {
+                val dbManager = DBManager(this.contexto!!)
+                val selectionArgs = arrayOf(nota.notaID.toString())
+                dbManager.borrar("ID=?", selectionArgs)
+                cargarQuery("%")
+            }
+
+            miVista.imageViewEditar.setOnClickListener {
+                val intent = Intent(this@MainActivity, AddActivity::class.java)
+                intent.putExtra("ID", nota.notaID)
+                intent.putExtra("titulo", nota.titulo)
+                intent.putExtra("descripcion", nota.descripcion)
+                intent.putExtra("control", 0)
+                startActivity(intent)
+            }
+
             return miVista
         }
     }
